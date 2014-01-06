e8e559412f7df7d69fc37323f44cecfc764320a7
takuma sato <innovationprograming@gmail.com>
2014-01-07 06:46:19 +0900

    余計なのものが表示されないよう

--- bat/run.bat
+++ bat/run.bat
@@ -6,13 +6,16 @@ pushd %JAVA_PATH%
 IF %1==toc (
 @echo ON
 java -classpath %ROOT_PATH%\out\  %2 %3 %4 %5 %6 %7 %8 %9
+@echo OFF
 ) ELSE IF %1==mes (
 @echo ON
 java -classpath %ROOT_PATH%\out\ MesMan.MesMan %2 %3 %4 %5 %6 %7 %8 %9
+@echo OFF
 ) ELSE IF %1==help (
 @echo Enable command
 @echo   toc
 @echo   mes
 @echo e.g run toc
+@echo OFF
 )
 popd
