e28a97f20c9c628638df5e523284cb1cb12cffc4
namespaceKYOTO <namespaceKYOTO@gamil.com>
2014-06-25 05:52:01 +0900

    ヘルプクラス追加。

---
+++ project/MessageManager/src/MesMan/Help.java
@@ -0,0 +1,14 @@
+package MesMan;
+
+/**
+ * ヘルプ.
+ * @author t_sato
+ *
+ */
+public class Help
+{
+//    JOptionPane.showMessageDialog(this, "JOptionPane");
+	// ようこそ　-> Dialog標示
+	// ヘルプ目次　-> Dialog標示
+	// テストデータ -> テストデータ標示
+}
